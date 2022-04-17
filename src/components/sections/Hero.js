22e0ac87fc6577f20433613959eb5ad73e2afa4a
shaka <shaka>
2022-04-17 11:33:46 -0500

    remove description

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -71,11 +71,6 @@ const Hero = ({
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="10">
               Imani Blooms
             </h1>
-            <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="30">
-                Flower service growing in Detroit with joy
-                </p>
-            </div>
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="50">
           <Image
